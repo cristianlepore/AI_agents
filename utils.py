7e7b98b4242c4169cb330278b1692b72af301ed2
DevOps User <devops@example.com>
2026-03-24 10:14:06 +0100

    add requirements.txt

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 from pathlib import Path
 
+BASE_DIR = Path.cwd()
+
 def resolve_abs_path(path_str: str) -> Path:
     path = Path(path_str).expanduser()
+
     if not path.is_absolute():
-        path = (Path.cwd() / path).resolve()
+        path = (BASE_DIR / path).resolve()
+
     return path
